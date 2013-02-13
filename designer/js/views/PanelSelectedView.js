319bd3074dbe13d689cdb8612673bd447623ef43
Zhou Xinzi <imdreamrunner@gmail.com>
2013-02-13 12:50:17 +0800

    Improve hover view

--- designer/js/views/PanelSelectedView.js
+++ designer/js/views/PanelSelectedView.js
@@ -1,4 +1,4 @@
-var PanelSelectedView = PanelHoverView.extend({
+var PanelSelectedView = Backbone.View.extend({
   el: '<div id="object-selected">',
 
   initialize: function(){
@@ -18,6 +18,33 @@ var PanelSelectedView = PanelHoverView.extend({
 
   events: {
     'mousedown': 'mouseDown'
+  },
+
+  render: function(){
+    var x, y, width, height;
+
+    if(this.model.get('_cropping')){
+      var widthScale = this.model.get('width') / this.model.get('sWidth');
+      var heightScale = this.model.get('height') / this.model.get('sHeight');
+      x = this.model.get('x') - widthScale * this.model.get('sX');
+      y = this.model.get('y') - heightScale * this.model.get('sY');
+      width = this.model.get('_originalWidth') * widthScale;
+      height = this.model.get('_originalHeight') * heightScale;
+    }else{
+      x = this.model.get('x');
+      y = this.model.get('y');
+      width = this.model.get('_actualWidth') || this.model.get('width');
+      height = this.model.get('_actualHeight') || this.model.get('height');
+    }
+
+    this.$el.css({
+      'margin-left': parseInt(x * designer.scale) -1,
+      'margin-top': parseInt(y * designer.scale) - 1,
+      width: parseInt(width * designer.scale),
+      height: parseInt(height * designer.scale)
+    });
+
+    this.$el.selectLess();
   },
 
   selectedRender: function(){
