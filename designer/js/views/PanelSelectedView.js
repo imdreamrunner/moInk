1300572e20020683a8266962b625cf96555b8c85
Zhou Xinzi <imdreamrunner@gmail.com>
2013-02-26 23:07:27 +0800

    Change resize behaviour when cropping

--- designer/js/views/PanelSelectedView.js
+++ designer/js/views/PanelSelectedView.js
@@ -37,6 +37,7 @@ var PanelSelectedView = Backbone.View.extend({
     var rotate = this.model.get('rotate') || 0;
 
     if(this.model.get('_cropping')){
+      /* For Whole Pic */
       var sWidth = this.model.get('sWidth');
       var sHeight = this.model.get('sHeight');
       var sX = this.model.get('sX');
@@ -51,8 +52,6 @@ var PanelSelectedView = Backbone.View.extend({
       var rX = (2 * sX - originalWidth + sWidth) * widthScale / 2;
       var rY = (2 * sY - originalHeight + sHeight) * heightScale / 2;
 
-      console.log('rX = ' + rX + ', rY = ' + rY);
-
       this.$el.css({
         'margin-left': parseInt((x - originalWidth * widthScale / 2 - (cosR * rX - sinR * rY)) * designer.scale) - 1,
         'margin-top': parseInt((y - originalHeight * heightScale / 2 - (sinR * rX + cosR * rY)) * designer.scale) - 1,
@@ -61,6 +60,7 @@ var PanelSelectedView = Backbone.View.extend({
       });
 
     }else{
+      /* For displayable pic */
       this.$el.css({
         'margin-left': parseInt((x - width / 2) * designer.scale) - 1,
         'margin-top': parseInt((y - height / 2) * designer.scale) - 1,
@@ -85,6 +85,10 @@ var PanelSelectedView = Backbone.View.extend({
       this.originY = this.model.get('y');
       this.originWidth = this.model.get('width');
       this.originHeight = this.model.get('height');
+      this.originSX = this.model.get('sX');
+      this.originSY = this.model.get('sY');
+      this.originSWidth = this.model.get('sWidth');
+      this.originSHeight = this.model.get('sHeight');
       $(document.body).on('mousemove', this.doResize);
       $(document.body).on('mouseup', this.endResize);
     }else{
@@ -132,41 +136,108 @@ var PanelSelectedView = Backbone.View.extend({
     var rX = cosR * newX + sinR * newY;
     var rY = - sinR * newX + cosR * newY;
 
-    if(this.resize === 'top-handler'
-      || this.resize === 'right-top-handler'
-      || this.resize === 'left-top-handler'){
-      height = parseInt(this.originHeight / 2 - rY);
-      x = parseInt(x - (rY + this.originHeight / 2) * sinR / 2);
-      y = parseInt(y + (rY + this.originHeight / 2) * cosR / 2);
-    }
-    if(this.resize === 'bottom-handler'
-      || this.resize === 'right-bottom-handler'
-      || this.resize === 'left-bottom-handler'){
-      height = parseInt(this.originHeight / 2 + rY);
-      x = parseInt(x - (rY - this.originHeight / 2) * sinR / 2);
-      y = parseInt(y + (rY - this.originHeight / 2) * cosR / 2);
-    }
-    if(this.resize === 'left-handler'
-      || this.resize === 'left-bottom-handler'
-      || this.resize === 'left-top-handler'){
-      width = parseInt(this.originWidth / 2 - rX);
-      x = parseInt(x + (rX + this.originWidth / 2) * cosR / 2);
-      y = parseInt(y + (rX + this.originWidth / 2) * sinR / 2);
-    }
-    if(this.resize === 'right-handler'
-      || this.resize === 'right-bottom-handler'
-      || this.resize === 'right-top-handler'){
-      width = parseInt(this.originWidth / 2 + rX);
-      x = parseInt(x + (rX - this.originWidth / 2) * cosR / 2);
-      y = parseInt(y + (rX - this.originWidth / 2) * sinR / 2);
-    }
+    if(this.model.get('_cropping')){
+      /* Resize when cropping */
+      var sX = this.originSX;
+      var sY = this.originSY;
+      var sWidth = this.originSWidth;
+      var sHeight = this.originSHeight;
+      var originalWidth = this.model.get('_originalWidth');
+      var originalHeight = this.model.get('_originalHeight');
+      var widthScale = width / sWidth;
+      var heightScale = height / sHeight;
+      var changeX, changeY, changeWidthScale, changeHeightScale;
 
-    this.model.set({
-      x: x,
-      y: y,
-      width: width,
-      height: height
-    });
+      if(this.resize === 'top-handler'
+        || this.resize === 'right-top-handler'
+        || this.resize === 'left-top-handler'){
+        changeY = rY + this.originHeight / 2 + sY * heightScale;
+        changeHeightScale = originalHeight / (originalHeight  - changeY / heightScale);
+        if(changeHeightScale < originalHeight / (originalHeight - sY)){
+          /* Within area */
+          sY = parseInt(sY * changeHeightScale - changeY / heightScale * changeHeightScale);
+          sHeight = parseInt(sHeight * changeHeightScale);
+        }else{
+          /* Out of area */
+          sHeight = parseInt(sHeight * originalHeight / (originalHeight - sY));
+          sY = 0;
+        }
+      }
+      if(this.resize === 'bottom-handler'
+        || this.resize === 'right-bottom-handler'
+        || this.resize === 'left-bottom-handler'){
+        changeY = rY - this.originHeight / 2 - (originalHeight - sHeight - sY) * heightScale;
+        changeHeightScale = Math.min(originalHeight / (originalHeight + changeY / heightScale),
+          originalHeight / (sY + sHeight));
+        sY = parseInt(sY * changeHeightScale);
+        sHeight = parseInt(sHeight * changeHeightScale);
+      }
+      if(this.resize === 'left-handler'
+        || this.resize === 'left-bottom-handler'
+        || this.resize === 'left-top-handler'){
+        changeX = rX + this.originWidth / 2 + sX * widthScale ;
+        changeWidthScale = originalWidth / (originalWidth - changeX / widthScale);
+        if(changeWidthScale < originalWidth / (originalWidth - sX)){
+          sX = parseInt(sX * changeWidthScale - changeX / widthScale * changeWidthScale);
+          sWidth = parseInt(sWidth * changeWidthScale);
+        }else{
+          sWidth = parseInt(sWidth * originalWidth / (originalWidth - sX));
+          sX = 0;
+        }
+      }
+      if(this.resize === 'right-handler'
+        || this.resize === 'right-bottom-handler'
+        || this.resize === 'right-top-handler'){
+        changeX = rX - this.originWidth / 2 - (originalWidth - sWidth - sX) * widthScale ;
+        changeWidthScale = Math.min(originalWidth / (originalWidth + changeX / widthScale),
+          originalWidth / (sX + sWidth));;
+        sX = parseInt(sX * changeWidthScale);
+        sWidth = parseInt(sWidth * changeWidthScale);
+      }
+      this.model.set({
+        sX: sX,
+        sY: sY,
+        sWidth: sWidth,
+        sHeight: sHeight
+      });
+    }else{
+      /* Normal Resize */
+      if(this.resize === 'top-handler'
+        || this.resize === 'right-top-handler'
+        || this.resize === 'left-top-handler'){
+        height = parseInt(this.originHeight / 2 - rY);
+        x = parseInt(x - (rY + this.originHeight / 2) * sinR / 2);
+        y = parseInt(y + (rY + this.originHeight / 2) * cosR / 2);
+      }
+      if(this.resize === 'bottom-handler'
+        || this.resize === 'right-bottom-handler'
+        || this.resize === 'left-bottom-handler'){
+        height = parseInt(this.originHeight / 2 + rY);
+        x = parseInt(x - (rY - this.originHeight / 2) * sinR / 2);
+        y = parseInt(y + (rY - this.originHeight / 2) * cosR / 2);
+      }
+      if(this.resize === 'left-handler'
+        || this.resize === 'left-bottom-handler'
+        || this.resize === 'left-top-handler'){
+        width = parseInt(this.originWidth / 2 - rX);
+        x = parseInt(x + (rX + this.originWidth / 2) * cosR / 2);
+        y = parseInt(y + (rX + this.originWidth / 2) * sinR / 2);
+      }
+      if(this.resize === 'right-handler'
+        || this.resize === 'right-bottom-handler'
+        || this.resize === 'right-top-handler'){
+        width = parseInt(this.originWidth / 2 + rX);
+        x = parseInt(x + (rX - this.originWidth / 2) * cosR / 2);
+        y = parseInt(y + (rX - this.originWidth / 2) * sinR / 2);
+      }
+
+      this.model.set({
+        x: x,
+        y: y,
+        width: width,
+        height: height
+      });
+    }
   },
 
   close: function(){
